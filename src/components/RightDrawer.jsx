3fcc64ace3b0428c70786432ecb52d6a85f0a25a
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-05-25 23:40:00 +1000

    inputs added into drawer

--- src/components/RightDrawer.jsx
+++ src/components/RightDrawer.jsx
@@ -1,23 +1,18 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import React, { useState } from 'react';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import Box from '@mui/material/Box';
 
 export default function RightDrawer() {
-  const [state, setState] = React.useState({
+  const [state, setState] = useState({
     right: true,
+    taskTitle: '',
+    taskDescription: '',
   });
 
   const toggleDrawer = (anchor, open) => event => {
     if (
+      event &&
       event.type === 'keydown' &&
       (event.key === 'Tab' || event.key === 'Shift')
     ) {
@@ -27,33 +22,66 @@ export default function RightDrawer() {
     setState({ ...state, [anchor]: open });
   };
 
+  const handleInputChange = event => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    // Perform necessary actions with the updated task data (e.g., update the page, store changes in local storage)
+
+    // Reset the form inputs and close the drawer
+    setState({
+      ...state,
+      taskTitle: '',
+      taskDescription: '',
+      right: false,
+    });
+  };
+
   const list = anchor => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      sx={{ width: 250 }}
       role='presentation'
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <h1>hello</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Task Title:
+          <input
+            type='text'
+            name='taskTitle'
+            value={state.taskTitle}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Task Description:
+          <textarea
+            name='taskDescription'
+            value={state.taskDescription}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <Button type='submit'>Update Task</Button>
+      </form>
     </Box>
   );
 
   return (
     <div>
-      {['left', 'right', 'top', 'bottom'].map(anchor => (
-        <>
-          <Button key={anchor} onClick={toggleDrawer(anchor, true)}>
-            {anchor}
-          </Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </>
-      ))}
+      <Button onClick={toggleDrawer('right', true)}>Edit Task</Button>
+      <Drawer
+        anchor='right'
+        open={state.right}
+        onClose={toggleDrawer('right', false)}
+      >
+        {list('right')}
+      </Drawer>
     </div>
   );
 }
